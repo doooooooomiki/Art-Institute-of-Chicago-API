ca75344ceb2d2c80bd08fff14596a76f6d69af44
doooooooomiki <dominic.kirste@gmail.com>
2024-07-07 20:12:36 +0200

    ✨

--- src/api/artworks.ts
+++ src/api/artworks.ts
@@ -1,9 +1,29 @@
-import { ofetch } from 'ofetch'
-
 export async function getArtworks() {
-  return await ofetch('https://api.artic.edu/api/v1/artworks?limit=2')
+  const url = 'https://api.artic.edu/api/v1/artworks?limit=2'
+
+  const cache = await window.caches.open('v1')
+
+  const match = await cache.match(url)
+
+  if (match) {
+    return match.json()
+  } else {
+    await cache.add(url)
+    return (await cache.match(url))?.json()
+  }
 }
 
 export async function getArtworkById(id: string) {
-  return await ofetch(`https://api.artic.edu/api/v1/artworks/${id}`)
+  const url = `https://api.artic.edu/api/v1/artworks/${id}`
+
+  const cache = await window.caches.open('v1')
+
+  const match = await cache.match(url)
+
+  if (match) {
+    return match.json()
+  } else {
+    await cache.add(url)
+    return (await cache.match(url))?.json()
+  }
 }
